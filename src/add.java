30902990276bb35c11e474711ccaf5f9e9289f21
rohini <rohinisengodan0510@gmail.com>
2025-08-15 20:50:14 +0530

    15/08/2025

--- src/add.java
+++ src/add.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class add {
     public static void main(String[] args) {
-
+    Scanner s = new Scanner(System.in);
+        System.out.print("Enter a num1:");
+        int a = s.nextInt();
+        System.out.print("Enter a num2:");
+        int b = s.nextInt();
+        int c = a+b;
+        System.out.println("sum of the num "+c);
     }
 }
