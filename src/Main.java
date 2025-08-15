15f60646940f80ae4e103933d6dd8a29bab9d89d
rohini <rohinisengodan0510@gmail.com>
2025-08-15 20:40:26 +0530

    15/08/2025

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Main{
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
-        int a = s.nextInt();//first number podanum
-        int b = s.nextInt();//second num podanum
+        int a = s.nextInt();
+        int b = s.nextInt();
         if(a>b)
             System.out.println("a is greater");
         else
